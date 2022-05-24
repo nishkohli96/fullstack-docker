9883d19a73f4fabe236d60bfe6d89786ac80becf
Nishant Kohli <nishkohli96@gmail.com>
2022-05-24 20:33:07 +0530

    render UI

---
+++ packages/frontend/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import { RootDiv } from '@shared/ui';
+const HomePage: FC = () => {
+	return (
+		<RootDiv>
+			<span>hi from home</span>
+		</RootDiv>
+	);
+};
+
+export default HomePage;
---
+++ packages/shared-ui/src/common/StyledComps.tsx
@@ -0,0 +1,7 @@
+import { styled } from '@mui/material';
+
+export const RootDiv = styled('div')({
+	backgroundColor: 'powderblue',
+	width: '100vw',
+	minHeight: '100vh',
+});
