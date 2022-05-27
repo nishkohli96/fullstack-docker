045e8bafc2f1a39acce5d9bceb18674ea815c370
Nishant Kohli <nishkohli96@gmail.com>
2022-05-27 06:12:12 +0530

    RHF

--- packages/frontend/src/pages/home/index.tsx
+++ packages/frontend/src/pages/home/index.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react';
 import { RootDiv } from '@shared/ui';
+import RHF from './RHF';
+
 const HomePage: FC = () => {
 	return (
 		<RootDiv>
-			<span>hi from home</span>
+			<div style={{ padding: '30px 20px' }}>
+				<p>Hi from home</p>
+				<RHF />
+			</div>
 		</RootDiv>
 	);
 };
