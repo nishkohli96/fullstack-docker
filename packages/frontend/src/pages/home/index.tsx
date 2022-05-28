e6bfe4753abf155105cc86f9fd49aa33b0dbe9f1
Nishant Kohli <nishkohli96@gmail.com>
2022-05-28 05:55:50 +0530

    test build dockerfile

--- packages/frontend/src/pages/home/index.tsx
+++ packages/frontend/src/pages/home/index.tsx
@@ -1,17 +1,17 @@
 import { FC } from 'react';
-import { RootDiv } from '@shared/ui';
+// import { RootDiv } from '@shared/ui';
 import RHF from './RHF';
 
 const HomePage: FC = () => {
 	const { REACT_APP_TESTVAR } = process.env;
 	return (
-		<RootDiv>
-			<div style={{ padding: '30px 20px' }}>
-				<p>Hi from home</p>
-				<p>env var - "{REACT_APP_TESTVAR}"</p>
-				<RHF />
-			</div>
-		</RootDiv>
+		// <RootDiv>
+		<div style={{ padding: '30px 20px' }}>
+			<p>Hi from home</p>
+			<p>env var - "{REACT_APP_TESTVAR}"</p>
+			<RHF />
+		</div>
+		// </RootDiv>
 	);
 };
 
